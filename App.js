c36bd6df6de0ca6ca3f31c6bc11f96f00068010f
Rui Silva <rui.leal.da.silva@gmail.com>
2023-03-16 23:02:28 +0000

    Still has issues

--- App.js
+++ App.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import { StyleSheet, View } from "react-native";
 import Header from "./components/Header";
 import SudokuBoard from "./components/SudokuBoard";
-// import SudokuGenerator from "./components/SudokuGenerator";
 import Timer from "./components/Timer";
 import FinishModal from "./components/FinishModal";
 import generateBoard from "./utils/generateBoard";
@@ -11,32 +10,37 @@ function compareArrays(arr1, arr2) {
   return JSON.stringify(arr1) === JSON.stringify(arr2);
 }
 
-const { puzzle, solution } = generateBoard("test");
+const emptyBoard = Array.from({ length: 9 }, () =>
+  Array.from({ length: 9 }, () => 0)
+);
 
-// Make sure generateExports both board and solution
-// Next allow for the already filled numbers to be changed
 // Make the cross disappear after input
 // Change cross color and make the center another color so it's easier to see
-// Take care of the Modal Logic To properly display a winner
 // Next steps: difficulty and maybe high score.
 
 export default function App() {
-  const [isFirstTime, setFirstTime] = useState(true);
-  const [board, setBoard] = useState(puzzle);
-  // const [solution, setSolution] = useState(solution);
+  const [board, setBoard] = useState(emptyBoard);
+  const [levelSolution, setLevelSolution] = useState(false);
   const [time, setTime] = useState(0);
   const [isTimerRunning, setIsTimerRunning] = useState(false);
   const [isFinishModalVisible, setIsFinishModalVisible] = useState(false);
 
+  const handleNewGame = () => {
+    const { puzzle, solution } = generateBoard("test");
+    setTime(0);
+    setIsTimerRunning(true);
+    setBoard(puzzle);
+    setLevelSolution(solution);
+  };
+
   useEffect(() => {
+    handleNewGame();
+  }, []);
+
+  useEffect(() => {
+    console.log("RELOAD");
     const isBoardFilled = board.every((row) => row.every((cell) => cell !== 0));
-
-    const isBoardCorrect = isBoardFilled && compareArrays(board, solution);
-
-    if (isFirstTime) {
-      setFirstTime(false);
-      setIsTimerRunning(true);
-    }
+    const isBoardCorrect = isBoardFilled && compareArrays(board, levelSolution);
 
     if (isBoardCorrect) {
       // Board is solved
@@ -46,13 +50,7 @@ export default function App() {
       // Board is not solved
       // Do something else, like show an error message or reset the board
     }
-  }, [board, isFirstTime]);
-
-  const handleNewGame = () => {
-    setTime(0);
-    setIsTimerRunning(true);
-    setBoard(puzzle);
-  };
+  }, [board]);
 
   const handleTimerTick = () => {
     setTime((prevTime) => prevTime + 1);
